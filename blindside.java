7f782786fec7633497825abb13c98bccdc58bf0d
amwittenberg <wittenberg.18@osu.edu>
2013-06-24 17:04:22 -0300

    Update blindside.java
    So far a panel should pop up that has the capability of adding names to it and hitting done

--- blindside.java
+++ blindside.java
@@ -1,8 +1,37 @@
 // Project: BlindSide
 // Authors: Ryan Coble, Annelise Wittenberg, Nkenge Cameron, Nikki Grinberg, Edgar Uribe, Tre Harding
 
-// WHY DO WE SUCK
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-// hi
+@SuppressWarnings("serial")
+public class BlindSide extends JFrame implements ActionListener{
 
-// Testing :o
+  // Some random thing so it stops having an error
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	// Fields
+	JPanel namepanel;
+	JButton donebutt;
+	JButton addbutt;
+	JLabel bs;
+	
+	// Constructor
+	public BlindSide() {
+		namepanel = new JPanel();
+		donebutt = new JButton();
+		addbutt = new JButton();
+		bs = new JLabel();
+		// Ability to add names
+		namepanel.add(addbutt);
+		namepanel.add(donebutt);
+		this.add(namepanel);
+	}
+	
+	
+}
